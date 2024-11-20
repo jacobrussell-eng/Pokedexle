12843919feee73d2042b22eac23a8296d32ac7bd
Jacob Russell <8bitjacob@gmail.com>
2024-11-20 12:51:50 +0000

    div creation demo
    using grid layouts and createElement to fill the game space with letterboxes, can only be tested in deployment so hopefully works

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // var init:
 let nameArray = [];
+let ran;
+let currentMon;
+let monLength;
 
 const nameRequest = new Request('pokemon_names.txt');
 function fetchNames() {
@@ -17,15 +20,33 @@ function fetchNames() {
     })
 }
 
-
 // HTML connectors:
 const startOverlay = document.querySelector("#startScreen");
 const gameSpace = document.querySelector("#gameSpace");
+const textSpace = document.querySelector("#textSpace");
 
+// make letterboxes:
+function generateLetterBoxes() {
+    monLength = currentMon.length();
+    gameSpace.style.gridTemplate = "repeat(6, 1fr) / repeat(${monLength}, 1fr)";
+    for (var r = 0; r < 6; r++) {
+        for (var c = 0; c < monLength; c++) {
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('letterbox');
+            newDiv.id = "letter-r${r+1}-c${c+1}";
+            newDiv.textContent = "r${r+1}-c${c+1}";
+        }
+    }
+}
 
 // start game:
 function startGame() {
     startOverlay.style.display = "none";
-    gameSpace.style.display = "block";
+    gameSpace.style.display = "grid";
     fetchNames();
+    ran = Math.floor(Math.random()*809);
+    currentMon = nameArray[ran];
+    generateLetterBoxes();
+    // textSpace.textContent = currentMon;
+    // console.log()
 }
