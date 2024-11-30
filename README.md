ec1cbcc1fc9bb00333ad8b63424ffceb9cec06bc
Jacob Russell <8bitjacob@gmail.com>
2024-11-30 17:35:34 +0000

    major overhaul, semi-working state
    Got consistent letterbox generation, reads user keypresses, checks at the end of the row, and has a successful win state.
    
    Next to-do: Finish lose state, polish game restart, program backspace function, tidy up UI
    
    Stretch goals: Pull names from PokeAPI? Add 'Hints' using API data? Other features/difficulties?

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
  Wordle clone using Pokemon names
 
 ## To-do:
-- [ ] Generate Letterboxes across 6 rows and n columns (n = name length)
-- [ ] Allow user input, one box at a time
-- [ ] Initiate end of line word checks, with yellow & green responses
-- [ ] Handle Win/Loss states
+- [ x ] Generate Letterboxes across 6 rows and n columns (n = name length)
+- [ x ] Allow user input, one box at a time
+- [ x ] Initiate end of line word checks, with yellow & green responses
+- [ x ] Handle Win/Loss states
 - [ ] Tidy UI
 - [ ] Design a Favicon
 - [ ] Optional: experiment with Pokemon data APIs? (Types, Pictures)
@@ -14,4 +14,5 @@
 Pokemon Font: [CDN Fonts](https://www.cdnfonts.com/pokemon-solid.font)\
 Gameboy Font: [Fontspace](https://www.fontspace.com/gbboot-font-f2385)\
 List of Names: [ralts00 Gist](https://gist.github.com/ralts00/31415709fb34c1b2ec556c396efc3d80)\
-Fetch Text Example: [MDN Example](https://mdn.github.io/dom-examples/fetch/fetch-text/) & [Github Repo](https://github.com/mdn/dom-examples/tree/main/fetch/fetch-text)
+Fetch Text Example: [MDN Example](https://mdn.github.io/dom-examples/fetch/fetch-text/) & [Github Repo](https://github.com/mdn/dom-examples/tree/main/fetch/fetch-text)\
+Pokemon data API: [PokeAPI](https://pokeapi.co)
--- main.js
+++ main.js
@@ -1,12 +1,23 @@
+// python3 -m http.server 8000 in Pokedexle folder to host local http - thanks @benjaminbenben
+
 // var init:
 let nameArray = [];
 let ran;
-let currentMon;
+let currentMon; // turn string to iterable for individual letterboxes?
 let monLength;
+let currentRow = 0;
+let currentCol = 0;
+let guessMatrix = []
 
+// HTML connectors:
+const startOverlay = document.querySelector("#startScreen");
+const gameSpace = document.querySelector("#gameSpace");
+const endScreen = document.querySelector("#endScreen");
+
+// Fetching names from the .txt:
 const nameRequest = new Request('pokemon_names.txt');
 async function fetchNames() {
-    fetch(nameRequest).then((response) => {
+    await fetch(nameRequest).then((response) => {
         console.log(response.ok);
         if (!response.ok) {
             console.log("Response error");
@@ -20,39 +31,115 @@ async function fetchNames() {
     })
 }
 
-// HTML connectors:
-const startOverlay = document.querySelector("#startScreen");
-const gameSpace = document.querySelector("#gameSpace");
-const textSpace = document.querySelector("#textSpace");
-
 // make letterboxes:
-function generateLetterBoxes(currentMon) {
-    monLength = currentMon.length();
-    gameSpace.style.gridTemplate = "repeat(6, 1fr) / repeat(${monLength}, 1fr)";
+function generateLetterBoxes() {
+    monLength = currentMon.length;
+    gameSpace.style.gridTemplate = `repeat(6, max(10vh,50px)) / repeat(${monLength}, max(10vh,50px))`;
+    gameSpace.style.width = `max(${monLength*10}vh,${monLength*50}px)`;
+    gameSpace.style.height = `max(60vh,300px)`;
     for (var r = 0; r < 6; r++) {
         for (var c = 0; c < monLength; c++) {
             const newDiv = document.createElement('div');
             newDiv.classList.add('letterbox');
-            newDiv.id = "letter-r${r}-c${c}";
-            newDiv.textContent = "r${r}-c${c}";
+            newDiv.id = `letter-r${r}-c${c}`;
+            newDiv.textContent = `r${r}-c${c}`;
 
             gameSpace.appendChild(newDiv);
         }
     }
+    guessMatrix = Array.from({length:6},() => Array.from({length:monLength}, ()=>"")); // thx ben
+}
+
+// user input function:
+const inputHandler = (event) => {
+    if (event.key.match(/[a-zA-Z]/)) {
+        letterInsert(event.key.toUpperCase());
+    }
+    else if (event.key == "Backspace") {
+        letterDelete();
+    }
+}
+
+
+function letterInsert(letter){
+    guessMatrix[currentRow][currentCol] = letter;
+    document.getElementById(`letter-r${currentRow}-c${currentCol}`).textContent = letter;
+    console.log(guessMatrix);
+    if (currentCol != (monLength-1)) {
+        currentCol++;
+    } else {
+        let correctGuesses = checkRow();
+        console.log("correctGuesses = " + correctGuesses)
+        if (correctGuesses == monLength) {
+            winGame();
+        } else {
+            currentRow++ , currentCol=0
+        }
+    }
+}
+
+function letterDelete(){}
+
+
+function checkRow() {
+    let perfectLetters = 0
+    rowToCheck = guessMatrix[currentRow];
+    answerArray = currentMon.toUpperCase().split('');
+    console.log("row to check: " + rowToCheck)
+    console.log("answerArray: " + answerArray)
+    for (i=0;i<rowToCheck.length;i++) {
+        if (rowToCheck[i] == answerArray[i]) {
+            document.getElementById(`letter-r${currentRow}-c${i}`).style.backgroundColor = "green";
+            perfectLetters++;
+        } else if (answerArray.includes(rowToCheck[i]) && rowToCheck[i] != answerArray[i]) {
+            document.getElementById(`letter-r${currentRow}-c${i}`).style.backgroundColor = "yellow";
+        } 
+        // else {
+        //     allTrue = false;
+        // }
+    }
+    return perfectLetters;
+}
+
+function winGame() {
+    window.removeEventListener("keypress", inputHandler);
+    // HTML win screen:
+    endScreen.style.display = "flex";
+    endScreen.textContent = `Congrats! You guessed ${currentMon} in ${currentRow+1} tries!`;
+
+    // Restart Button:
+    const restartButton = document.createElement('button');
+    restartButton.textContent = "Play Again?";
+    restartButton.addEventListener("click", startGame);
+    endScreen.appendChild(restartButton);
+}
+
+function loseGame() {
+    window.removeEventListener("keypress", inputHandler);
+    // HTML win screen:
+    endScreen.style.display = "flex";
+    endScreen.textContent = `Ach! Unfortunately you didn't guess the Pokemon`;
+
+    // Restart Button:
+    const restartButton = document.createElement('button');
+    restartButton.textContent = "Try Again?";
+    restartButton.addEventListener("click", startGame);
+    endScreen.appendChild(restartButton);
 }
 
 // start game:
 async function startGame() {
     startOverlay.style.display = "none";
+    endScreen.style.display = "none";
     gameSpace.style.display = "grid";
     await fetchNames();
+    window.addEventListener("keypress", inputHandler);
 
     ran = Math.floor(Math.random()*809);
     console.log("random num 0-809: ", ran);
     currentMon = nameArray[ran];
     console.log("current mon: ", currentMon);
-    generateLetterBoxes(currentMon);
+    generateLetterBoxes();
 
-    // textSpace.textContent = currentMon;
     // console.log()
 }
